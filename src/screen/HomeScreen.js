291fac992c44c7de22ca027886ce1524c027313a
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:33:59 +0700

    Upload Code

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,20 +7,30 @@ import {
   TouchableOpacity,
   Image,
   ActivityIndicator,
-} from 'react-native';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { ScrollView } from 'react-native';
-import Resep from '../components/Resep';
-import bg from '../../img/bg.jpg';
-import useHttp from '../hooks/use-http';
-import { useAuth } from '../context/auth-context';
+} from "react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { ScrollView } from "react-native";
+import Resep from "../components/Resep";
+import bg from "../../img/bg.jpg";
+import useHttp from "../hooks/use-http";
+import { useAuth } from "../context/auth-context";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const { user } = useAuth();
   const { isLoading, error, sendRequest } = useHttp();
+
+  const categories = ["Tradisional", "Hari Raya", "Camilan"];
+  const handleCategoryFilter = (selectedCategory) => {
+    // Lakukan logika pemfilteran resep berdasarkan kategori yang dipilih
+    const filteredRecipes = recipes.filter(recipe => recipe.category === selectedCategory);
+    
+    // Simpan hasil pemfilteran
+    setFilteredRecipes(filteredRecipes);
+  };
+  
 
   const filteredRecipes = useMemo(() => {
     return recipes.filter((recipe) =>
@@ -31,7 +41,7 @@ const HomeScreen = () => {
   const getRecipes = useCallback(async () => {
     sendRequest(
       {
-        url: '/api/v1/recipes',
+        url: "/api/v1/recipes",
       },
       (responseData) => {
         const data = responseData.data.data.map((recipe) => {
@@ -60,7 +70,7 @@ const HomeScreen = () => {
     sendRequest(
       {
         url: `/api/v1/recipes/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       },
       (responseData) => {
         console.log(responseData);
@@ -74,55 +84,52 @@ const HomeScreen = () => {
       <View style={styles.header}>
         <View style={styles.btn_container}>
           <TextInput
-            placeholderTextColor='#fff'
+            placeholderTextColor="#fff"
             style={styles.searchInput}
-            placeholder='Mau cari resep apa?'
+            placeholder="Mau cari resep apa?"
             onChangeText={(text) => setSearch(text)}
           />
           <TouchableOpacity
             style={styles.searchInput}
-            onPress={() => navigation.navigate('AddMakanan')}
+            onPress={() => navigation.navigate("AddMakanan")}
           >
-            <Text style={{ color: '#fff', fontWeight: 'bold' }}>
+            <Text style={{ color: "#fff", fontWeight: "bold" }}>
               + Tambah Makanan
             </Text>
           </TouchableOpacity>
         </View>
         <TouchableOpacity
           style={styles.profile}
-          onPress={() => navigation.navigate('AccUser')}
+          onPress={() => navigation.navigate("AccUser")}
         >
           <View>
             <View>
               <Image
-                source={require('../../img/foto.png')}
+                source={require("../../img/foto.png")}
                 style={styles.profileImage}
               />
             </View>
-            <Text style={styles.profileName}>{user.name || 'User'}</Text>
+            <Text style={styles.profileName}>{user.name || "User"}</Text>
           </View>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.rightContentCategory}>
-        <Text style={styles.contentTitle}>Category</Text>
-      </View>
       <View style={styles.categories}>
-        <TouchableOpacity style={styles.category}>
-          <Text style={styles.categoryText}>Tradisional</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.category}>
-          <Text style={styles.categoryText}>Hari Raya</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.category}>
-          <Text style={styles.categoryText}>Camilan</Text>
-        </TouchableOpacity>
+        {categories.map((category, index) => (
+          <TouchableOpacity
+            style={styles.category}
+            key={index}
+            onPress={() => handleCategoryFilter(category)}
+          >
+            <Text style={styles.categoryText}>{category}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
 
       <View style={styles.content}>
         <View style={styles.rightContent}>
           <Text style={styles.contentTitle}>Popular Recipes</Text>
-          {isLoading && <ActivityIndicator size='large' color='#DF0606' />}
+          {isLoading && <ActivityIndicator size="large" color="#DF0606" />}
           {!isLoading && (
             <ScrollView>
               {filteredRecipes?.map((recipe) => (
@@ -137,10 +144,10 @@ const HomeScreen = () => {
           {filteredRecipes.length === 0 && !isLoading && (
             <Text
               style={{
-                textAlign: 'center',
+                textAlign: "center",
                 marginTop: 20,
                 fontSize: 20,
-                fontWeight: 'bold',
+                fontWeight: "bold",
               }}
             >
               Tidak ada resep
@@ -156,12 +163,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 15,
-    backgroundColor: '#f0f8ff',
+    backgroundColor: "#f0f8ff",
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 50,
   },
   btn_container: {
@@ -178,50 +185,50 @@ const styles = StyleSheet.create({
     borderRadius: 70,
   },
   profileName: {
-    color: '#000',
+    color: "#000",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
-    textAlign: 'center',
+    textAlign: "center",
   },
   searchInput: {
     margin: 6,
     marginLeft: 0.5,
-    backgroundColor: '#A51B0B',
+    backgroundColor: "#A51B0B",
     borderRadius: 20,
     paddingVertical: 10,
     paddingHorizontal: 25,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
   content: {
     flex: 1,
   },
   contentTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   iconText: {
     marginLeft: 5,
-    color: 'gray',
+    color: "gray",
     fontSize: 14,
   },
   categories: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginVertical: 20,
   },
   category: {
-    backgroundColor: '#ed3101',
+    backgroundColor: "#ed3101",
     borderRadius: 60,
     paddingVertical: 10,
     paddingHorizontal: 20,
   },
   categoryText: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: 'white',
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "white",
   },
 });
 
